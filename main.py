2d108202546724f32de543d57f0ad4767b142627
周畅 <zhouchang@zhouchangs-MacBook-Pro-2.local>
2022-03-07 02:39:13 +0800

    benchmark holding control finish

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from Model1 import Model1
-
+#from Model1 import *
+import Model1 as mb
+import Model1_holding as mh
 if __name__=="__main__":
-    zc=Model1(M=8,N=24)
+    '''
+    zc=mb.Model1(M=8,N=20)
     #a=zc.Optimal()
     #a=zc()
     #print(a)
+    zc.Analysis()
+    '''
+    zc=mh.Model1(M=8,N=20)
     zc.Analysis()
